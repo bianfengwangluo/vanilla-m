61f5035c3a1b97ebb01f14a2181e9ded497af360
陈明旭 <amamiya_xu@163.com>
2021-09-15 15:20:17 +0800

    onload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import './src/assets/css/common.scss';
 
 const rootValue = 100;
-document.documentElement.style.fontSize = (rootValue * Math.min(1024, window.innerWidth)) / 750 + 'px';
+
+window.addEventListener('load', () => {
+  document.documentElement.style.fontSize = (rootValue * Math.min(1024, window.innerWidth)) / 750 + 'px';
+});
 
 window.addEventListener('resize', () => {
   document.documentElement.style.fontSize = (rootValue * Math.min(1024, window.innerWidth)) / 750 + 'px';
